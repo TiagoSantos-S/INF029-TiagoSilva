fedaab5ceb4c0b8ead74e5fc1c1eef140d124aab
Tainá Barbosa <160445609+Nabarbosa@users.noreply.github.com>
2026-02-18 19:16:57 -0300

    Add files via upload

---
+++ projeto_escola/projeto_escola.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define tam_alunos 3
+#define tam_professores 3
+
+int main(){
+    int opcao;
+
+    printf("Projeto Escola\n");
+    printf("0 - sair\n");
+    printf("1 - Aluno\n");
+    printf("2 - Professor\n");
+    printf("3 - Disciplina\n");
+    scanf("%d", &opcao);
+
+    switch (opcao){
+    case 0:
+        printf("Saindo do programa...\n");
+        break;
+    case 1:
+        printf("Opção Aluno selecionada.\n");
+        break;
+    case 2:
+        printf("Opção Professor selecionada.\n");
+        break;
+    case 3:
+        printf("Opção Disciplina selecionada.\n");
+        break;
+    default:
+        printf("Opção inválida. Tente novamente.\n");
+    }
+
+    return 0;
+}
